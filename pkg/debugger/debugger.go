3b2993da4cb158326d5c309ca8000e9f9ec54342
R4gn4r <r4gn4r.l0@gmail.com>
2022-11-16 12:04:01 +0000

    ADDMOD feature

--- pkg/debugger/debugger.go
+++ pkg/debugger/debugger.go
@@ -59,6 +59,12 @@ func (o *Debugger) executeCode(code byte) bool {
 		b := new(uint256.Int).SetBytes(o.stackPop())
 		x := new(uint256.Int).SMod(a, b)
 		o.stackPush(x.Bytes())
+	case code == 0x08:
+		a := new(uint256.Int).SetBytes(o.stackPop())
+		b := new(uint256.Int).SetBytes(o.stackPop())
+		c := new(uint256.Int).SetBytes(o.stackPop())
+		x := new(uint256.Int).AddMod(a, b, c)
+		o.stackPush(x.Bytes())
 	case code >= 0x60 && code <= 0x7f: // PUSHx
 		to := int16(code) - 0x5e
 		value := o.Bytecode[(o.ProgramCounter + 0x01):(o.ProgramCounter + to)]
